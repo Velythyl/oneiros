07d3db271b1b73646546f31d94df089b0af174a7
velythyl <charlie.gauthier@umontreal.ca>
2024-03-26 17:05:16 -0400

    fixed mapping

--- environments/wrappers/mappings/vector_index_rearrange.py
+++ environments/wrappers/mappings/vector_index_rearrange.py
@@ -93,6 +93,48 @@ class _Mapping:
         return obs[self._brax2obs]
 
 
+class Ant(_Mapping):
+    act: dict = {
+        0: 6,
+        1: 7,
+        2: 0,
+        3: 1,
+        4: 2,
+        5: 3,
+        6: 4,
+        7: 5
+    }
+
+    obs: dict = {
+        0: 0,
+        1: 2,
+        2: 3,
+        3: 4,
+        4: 1,
+        5: 5,
+        6: 6,
+        7: 7,
+        8: 8,
+        9: 9,
+        10: 10,
+        11: 11,
+        12: 12,
+        13: 13,
+        14: 14,
+        15: 15,
+        16: 16,
+        17: 17,
+        18: 18,
+        19: 19,
+        20: 20,
+        21: 21,
+        22: 22,
+        23: 23,
+        24: 24,
+        25: 25,
+        26: 26
+    }
+
 def map_func_lookup(mujoco_name: str) -> _Mapping:
     get_mapping_class = list(_Mapping.__subclasses__())
     get_mapping_class = {str(c).split(".")[-1].split("'")[0].lower(): c for c in get_mapping_class}

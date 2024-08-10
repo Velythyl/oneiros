de3f6480c88edf16e8ee47b7884dc3571ac807ac
velythyl <charlie.gauthier@umontreal.ca>
2024-08-09 20:00:04 -0400

    yucl

--- environments/config_utils.py
+++ environments/config_utils.py
@@ -179,7 +179,7 @@ def gen_eval_gamut(eval_cfgs):
 
         # todo make set_low and set_high part of the env config
         SET_LOW = 0.5
-        SET_HIGH = 10.0 #2.0 if "widow" in eval_cfgs["env_key"][0] else 10.0
+        SET_HIGH = 2.0 if "widow" in eval_cfgs["env_key"][0] else 10.0
         assert (GET_LOW == GET_HIGH == 1.0) or (GET_LOW == SET_LOW and GET_HIGH == SET_HIGH)
 
         SCALE_HIGH = 2
